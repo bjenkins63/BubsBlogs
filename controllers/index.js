9e5fe0fcd940a4b509de1afa66444e72d097f3b4
Brian Jenkins <studiojenkins@yahoo.com>
2021-03-30 12:24:44 -0400

    gutted for new functionality

--- controllers/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-const apiRoutes = require('/');
-const blogRoutes = require('/blogRoutes');
-
-router.use('/', blogRoutes);
-router.use('/api', apiRoutes);
-
-module.exports = router;
