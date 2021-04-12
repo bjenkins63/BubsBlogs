d023959f694ae9db86e02705eef0e0d522b9482c
Brian Jenkins <studiojenkins@yahoo.com>
2021-04-11 22:04:39 -0400

    changed route

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.listen(process.env.PORT || 3000);
 console.log(`Listening to server`);
 
 //landing page
-app.get('/', (req, res) => {
+app.get('/main', (req, res) => {
     res.render("main");
 })
 
